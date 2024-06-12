435571cf4259c6d27496dbe44a30779e22d71f81
vincenci <vincyvine558@gmail.com>
2024-06-12 13:02:18 +0300

    addae footer in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,32 @@ function App() {
   return (
     <div className="App">
      <h1> weather project</h1>
-     
+      <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/vincenci"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vincencia
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/vincenci/weather-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://lovely-pika-675f60.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
     </div>
   );
 }
