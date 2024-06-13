22af2ee99dc1212970c66efb811dbdc2976831c4
vincenci <vincyvine558@gmail.com>
2024-06-13 14:19:00 +0300

    added weather content

--- src/App.js
+++ src/App.js
@@ -3,8 +3,62 @@ import './App.css';
 
 function App() {
   return (
+  <div className='weatherProject'>
     <div className="App">
-     <h1> weather project</h1>
+     
+      <div className="Weather">
+        <form >
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+
+         <div className="WeatherInfo">
+      
+      
+      <div className="row mt-3">
+        <div className="col-6">
+          
+           <h1>paris</h1>
+      
+        <span>
+          wednesday 13 12:05
+        </span>
+        <span className="text-capitalize">rainy</span>
+        <br/>
+          
+            <span>Humidity: 80%</span>
+            <span>Wind: 30 km/h</span> 
+          
+        </div>
+        <div className="col-6">
+          <div className="d-flex">
+          <div>
+              🌦
+            </div>
+
+            <div>
+              16℃
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
       <footer>
           This project was coded by{" "}
           <a
@@ -31,6 +85,8 @@ function App() {
             hosted on Netlify
           </a>
         </footer>
+    </div>
+    </div>
     </div>
   );
 }
